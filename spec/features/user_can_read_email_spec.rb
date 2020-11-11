2561ebf3af6f3e7865aa3309223104ae8c8728b7
William <willspecker@gmail.com>
2020-11-11 16:17:01 +0100

    Writing test for navigation to inbox after sign in

--- spec/features/user_can_read_email_spec.rb
+++ spec/features/user_can_read_email_spec.rb
@@ -8,8 +8,13 @@ feature 'User can' do
         visit user_session_path
     end
 
-    it 'displays the inbox' do
+    it 'displays the inbox button' do
         expect(page).to have_content 'Inbox'
-        
+    end
+
+    it 'navigates to inbox' do
+        click_on 'Inbox'
+        expect(page).to have_content 'Sent'
+        expect(page).to have_content 'Trash'
     end
 end
