43aa28130e6753988f30974c36cfd115c4ba9039
William <willspecker@gmail.com>
2020-11-11 20:22:52 +0100

    Writing test for whether fbot can send and receive message

--- spec/features/user_can_read_email_spec.rb
+++ spec/features/user_can_read_email_spec.rb
@@ -8,13 +8,29 @@ feature 'User can' do
         visit user_session_path
     end
 
-    it 'displays the inbox button' do
+    it 'display the inbox button' do
         expect(page).to have_content 'Inbox'
     end
 
-    it 'navigates to inbox' do
+    it 'navigate to inbox' do
         click_on 'Inbox'
         expect(page).to have_content 'Sent'
         expect(page).to have_content 'Trash'
     end
 end
+
+feature 'User can' do
+    let(:sender) { FactoryBot.create(:user, name: 'Nametoo', email: 'sender@mail.com') }
+    let(:receiver) { FactoryBot.create(:user, name: 'Namethree', email: 'receiver@mail.com') }
+    before do
+        sender.send_message(receiver, 'A message', 'Subject')
+    end
+
+    it 'should have one new message in the inbox' do
+        count = receiver.mailbox.inbox.count
+        expect(count).to eq 1
+    end
+
+    
+end
+
